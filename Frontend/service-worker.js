229daf5f6a2208d9c14333a9587bda64f0ef0b0d
Dielee <me@linusdietz.eu>
2021-05-01 13:09:28 +0200

    Delete old workbox caches

--- Frontend/service-worker.js
+++ Frontend/service-worker.js
@@ -1,4 +1,4 @@
-var version = "0.5.2"
+var version = "0.5.3"
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
 workbox.setConfig({
@@ -27,4 +27,13 @@ addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
+});
+
+addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(keys => keys.filter(key => !key.endsWith(version)))
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+  );
 });
